6723903fad08a0a96c0435f42eab0bb4ec52f9aa
Travis Aucoin <travisdevaucoin@gmail.com>
2023-01-07 10:23:51 -0600

    views and tempaltes for catalog

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,4 +1,10 @@
 from django.urls import path
 from . import views
 
-urlpatterns = []
+urlpatterns = [
+    path("", views.index, name="index"),
+    # path("books/", views, name="books_list"),
+    # path("authors/", views, name="authors_list"),
+    # path("books/<int:pk>", views, name="books_detail"),
+    # path("authors/<int:pk>", views, name="authors_detail"),
+]
--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from .models import Author, Book, BookInstance, Genre
 
 # Create your views here.
+def index(request):
+    """View for homepage"""
+    # Generate counts for library objects
+    num_books = Book.objects.all().count()
+    num_book_instances = BookInstance.objects.all().count()
+    num_book_instances_available = BookInstance.objects.filter(
+        status__exact="a"
+    ).count()
+    num_authors = Author.objects.all().count()
+    num_genres = Genre.objects.all().count()
+    num_books_with_ipsum = Book.objects.filter(summary__contains="ipsum").count()
+
+    context = {
+        "num_books": num_books,
+        "num_book_instances": num_book_instances,
+        "num_book_instances_available": num_book_instances_available,
+        "num_authors": num_authors,
+        "num_genres": num_genres,
+        "num_books_with_ipsum": num_books_with_ipsum,
+    }
+
+    return render(request, "index.html", context=context)
